45b477ce823beeca913abca1d242e2426aae1514
dcordz <17937472+dcordz@users.noreply.github.com>
2025-02-15 14:10:54 -0500

    main - change app.sway.vote to sway.vote

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,14 @@
 (function () {
     "use strict"
 
-    fetch("https://app.sway.vote/up")
+    console.error = () => null;
+    console.warn = () => null;
+    console.log = () => null;
+    console.table = () => null;
+    console.info = () => null;
+    console.dir = () => null;
+
+    fetch("https://sway.vote/up")
         .then(() => console.log("Booting up app."))
         .catch((e) => null)
 })()
